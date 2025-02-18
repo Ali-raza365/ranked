00d410005c7a09e94ec758904cf59daff20ea85d
Raza Dev <59225711+Ali-raza365@users.noreply.github.com>
2025-02-18 15:38:58 +0500

    Fix: add IOS adUnitId

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,32 +1,33 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Platform, View, Keyboard } from 'react-native';
-import { BannerAd, BannerAdSize, TestIds, useForeground } from 'react-native-google-mobile-ads';
+import React, { useState, useRef, useEffect } from "react";
+import { Platform, View, Keyboard } from "react-native";
+import {
+  BannerAd,
+  BannerAdSize,
+  TestIds,
+  useForeground,
+} from "react-native-google-mobile-ads";
 
-const adUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : 'ca-app-pub-3647461067575621/4778977558';
+const adUnitId = __DEV__
+  ? TestIds.ADAPTIVE_BANNER
+  : Platform.OS === "ios"
+  ? "ca-app-pub-3647461067575621/8766888672" // Use actual iOS Ad Unit ID
+  : "ca-app-pub-3647461067575621/4778977558";
 
 function AdBanner() {
   const bannerRef = useRef<BannerAd>(null);
-  // const [isKeyboardVisible, setKeyboardVisible] = useState(false);
-
-  // useEffect(() => {
-  //   const showSubscription = Keyboard.addListener('keyboardDidShow', () => setKeyboardVisible(true));
-  //   const hideSubscription = Keyboard.addListener('keyboardDidHide', () => setKeyboardVisible(false));
-
-  //   return () => {
-  //     showSubscription.remove();
-  //     hideSubscription.remove();
-  //   };
-  // }, []);
-
   useForeground(() => {
-    Platform.OS === 'ios' && bannerRef.current?.load();
+    Platform.OS === "ios" && bannerRef.current?.load();
   });
 
   return (
     // !isKeyboardVisible && (
-      <View style={{ }}>
-        <BannerAd ref={bannerRef} unitId={adUnitId} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
-      </View>
+    <View style={{}}>
+      <BannerAd
+        ref={bannerRef}
+        unitId={adUnitId}
+        size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+      />
+    </View>
     // )
   );
 }
